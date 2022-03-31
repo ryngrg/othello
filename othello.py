da1b9caa3f44907dca5959a9eca7ce9808bd45c4
Aaryan Garg <73568701+ryngrg@users.noreply.github.com>
2022-03-31 02:46:58 -0400

    Update othello.py

--- othello.py
+++ othello.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tkinter as tk
 
 
 class Board:
@@ -13,77 +14,114 @@ class Board:
         self.end = False
 
     def possible_moves(self, player):
-        opp = 3 - player # opponent
+        opp = 3 - player  # opponent
         moves = []
         flip_list = []
-        n_moves = 0
         for i in range(8):
             for j in range(8):
                 if self.board[i][j] == 0:
+                    added = False
+                    f = []
+
                     # South
                     x = i + 1
                     while x < 8 and self.board[x][j] == opp:
                         x += 1
                     if (x < 8) and (x-i > 1) and (self.board[x][j] == player):
+                        added = True
                         moves.append((i, j))
-                        n_moves += 1
-                        f = []
                         for k in range(i + 1, x):
                             f.append((k, j))
-                        flip_list.append(f)
                     
                     # North
                     x = i - 1
                     while x >= 0 and self.board[x][j] == opp:
                         x -= 1
                     if (x >= 0) and (i-x > 1) and (self.board[x][j] == player):
-                        moves.append((i, j))
-                        n_moves += 1
-                        f = []
+                        if not added:
+                            added = True
+                            moves.append((i, j))
                         for k in range(x + 1, i):
                             f.append((k, j))
-                        flip_list.append(f)
                     
                     # East
                     y = j + 1
                     while y < 8 and self.board[i][y] == opp:
                         y += 1
                     if (y < 8) and (y-j > 1) and (self.board[i][y] == player):
-                        moves.append((i, j))
-                        n_moves += 1
-                        f = []
+                        if not added:
+                            added = True
+                            moves.append((i, j))
                         for k in range(j + 1, y):
                             f.append((i, k))
-                        flip_list.append(f)
                     
                     # West
                     y = j - 1
                     while y >= 0 and self.board[i][y] == opp:
                         y -= 1
                     if (y >= 0) and (j-y > 1) and (self.board[i][y] == player):
-                        moves.append((i, j))
-                        n_moves += 1
-                        f = []
+                        if not added:
+                            added = True
+                            moves.append((i, j))
                         for k in range(y + 1, j):
                             f.append((i, k))
-                        flip_list.append(f)
                     
                     # North - West
                     x = i - 1
                     y = j - 1
+                    while (y >= 0) and (x >= 0) and self.board[x][y] == opp:
+                        y -= 1
+                        x -= 1
+                    if (y >= 0) and (x >= 0) and (j-y > 1) and (self.board[x][y] == player):
+                        if not added:
+                            added = True
+                            moves.append((i, j))
+                        for k in range(1, j - y):
+                            f.append((i - k, j - k))
                     
                     # North - East
                     x = i - 1
                     y = j + 1
+                    while (y < 8) and (x >= 0) and self.board[x][y] == opp:
+                        y += 1
+                        x -= 1
+                    if (y < 8) and (x >= 0) and (y-j > 1) and (self.board[x][y] == player):
+                        if not added:
+                            added = True
+                            moves.append((i, j))
+                        for k in range(1, y - j):
+                            f.append((i - k, j + k))
                     
                     # South - West
                     x = i + 1
                     y = j - 1
+                    while (y >= 0) and (x < 8) and self.board[x][y] == opp:
+                        y -= 1
+                        x += 1
+                    if (y >= 0) and (x < 8) and (j-y > 1) and (self.board[x][y] == player):
+                        if not added:
+                            added = True
+                            moves.append((i, j))
+                        for k in range(1, j - y):
+                            f.append((i + k, j - k))
                     
                     # South - East
                     x = i + 1
                     y = j + 1
-                    
+                    while (y < 8) and (x < 8) and self.board[x][y] == opp:
+                        y += 1
+                        x += 1
+                    if (y < 8) and (x < 8) and (y-j > 1) and (self.board[x][y] == player):
+                        if not added:
+                            added = True
+                            moves.append((i, j))
+                        for k in range(1, y - j):
+                            f.append((i + k, j + k))
+
+                    if added:
+                        flip_list.append(f)
+
+        n_moves = len(moves)
         if n_moves == 0:
             return False
         self.n_moves = n_moves
@@ -105,10 +143,17 @@ class Board:
         elif not(self.possible_moves(self.player)):
             self.end = True
 
+    def count(self, p):
+        num = 0
+        for i in range(8):
+            for j in range(8):
+                if self.board[i][j] == p:
+                    num += 1
+        return num
+
     def play(self, get_move_func, display = False):
-        while not(self.end):
-            print(self.moves)
-            move = get_move_func(self.n_moves)
+        while not self.end:
+            move = get_move_func(self.player, self.n_moves, self.moves)
             self.make_move(move)
             if display:
                 self.display()
@@ -128,13 +173,24 @@ class Board:
             return 2
 
     def display(self):
+        # for i in range(8):
+        #     for j in range(8):
+        #         if
         print(self.board)
 
 
-def get_human_move(num_moves):
+def get_human_move(player, num_moves, moves):
+    print("player:", player, "\nmoves:", moves)
     i = int(input(str(num_moves) + " possible moves: "))
     return i
 
+
 if __name__ == "__main__":
-    board = Board()
-    board.play(get_human_move, display = True)
+    root = tk.Tk()
+    #frame = tk.Frame(root, width=240, height=240)
+    #frame.pack(expand=True, fill=tk.BOTH)
+    canvas = tk.Canvas(root, bg='green', width=240, height=240)
+    canvas.pack(expand=True, fill=tk.BOTH)
+    root.mainloop()
+    # board = Board()
+    # board.play(get_human_move, display=True)
