4e993fd2646dc9215ea6552fe76c778fa1ce82d6
Aaryan Garg <73568701+ryngrg@users.noreply.github.com>
2022-03-31 22:04:19 -0400

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,5 +1,9 @@
-from othello import Board
+class AI_Agent:
+    def __init__(self, player):
+        self.player = player
 
-class agent:
-    def __init__(self):
-        self.board = Board()
+    def get_move(self, board, player, n_moves, moves):
+        if player == self.player:
+            return 0
+        else:
+            return None
