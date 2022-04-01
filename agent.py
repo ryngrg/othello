c750b823d9d5dc4564e47daf5ccd6bfb34c21c8a
Aaryan Garg <73568701+ryngrg@users.noreply.github.com>
2022-04-01 00:14:08 -0400

    Update agent.py

--- agent.py
+++ agent.py
@@ -3,7 +3,7 @@ class AI_Agent:
         self.player = player
 
     def get_move(self, board, player, n_moves, moves):
-        if player == self.player:
+        if player == self.player and n_moves > 0:
             return 0
         else:
             return None
