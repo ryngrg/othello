5081c837dc412beb51d670148bd3a508b27b2249
Aaryan Garg <73568701+ryngrg@users.noreply.github.com>
2022-04-01 00:13:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,57 @@ from othello import Board
 from agent import AI_Agent
 import tkinter as tk
 
+
+def draw_bg_grid():
+    for i in range(9):
+        canvas.create_line(30 * i, 0, 30 * i, 240)
+        canvas.create_line(0, 30 * i, 240, 30 * i)
+    canvas.update()
+
+
+def disp_coins():
+    canvas.delete('all')
+    draw_bg_grid()
+    for i in range(8):
+        for j in range(8):
+            if board.board[i][j] == 1:
+                canvas.create_oval(3 + 30*i, 3 + 30*j, 27 + 30*i, 27 + 30*j, fill="black")
+            elif board.board[i][j] == 2:
+                canvas.create_oval(3 + 30*i, 3 + 30*j, 27 + 30*i, 27 + 30*j, fill="white")
+
+            if human[board.player - 1] and (i, j) in board.moves:
+                canvas.create_oval(12 + 30*i, 12 + 30*j, 18 + 30*i, 18 + 30*j, fill="yellow")
+    canvas.update()
+
+
+def game_over():
+    print("game over")
+    print("winner is :", board.winner)
+
+
+def click_func(event):
+    x = event.x
+    y = event.y
+    if not board.end:
+        if human[board.player - 1]:
+            i = int(x / 30)
+            j = int(y / 30)
+            if (i, j) in board.moves:
+                idx = board.moves.index((i, j))
+                board.make_move(idx)
+                disp_coins()
+                while not(board.end or human[board.player - 1]):
+                    bd, pl, n, mvs = board.get_curr_info()
+                    move = ai[board.player - 1].get_move(bd, pl, n, mvs)
+                    board.make_move(move)
+                    disp_coins()
+                if board.end:
+                    game_over()
+
+    else:
+        game_over()
+
+
 if __name__ == "__main__":
     human = [False, False]
     ai = [None, None]
@@ -14,16 +65,20 @@ if __name__ == "__main__":
     if r[0] in ('y', 'Y'):
         human[1] = True
     else:
-        ai[0] = AI_Agent(1)
+        ai[1] = AI_Agent(2)
     board = Board()
 
     root = tk.Tk()
-    canvas = tk.Canvas(root, bg='light green', width=240, height=240)
-    for i in range(9):
-        canvas.create_line(30 * i, 0, 30 * i, 240)
-        canvas.create_line(0, 30 * i, 240, 30 * i)
-    # canvas.create_oval(3, 3, 27, 27, fill="white")
-    # canvas.create_oval(33, 33, 57, 57, fill="black")
+    canvas = tk.Canvas(root, bg='green', width=240, height=240)
     canvas.pack(expand=True, fill=tk.BOTH)
-    root.mainloop()
 
+    canvas.bind("<Button-1>", click_func)
+
+    draw_bg_grid()
+    disp_coins()
+    if not human[0]:
+        bd, pl, n, mvs = board.get_curr_info()
+        move = ai[board.player - 1].get_move(bd, pl, n, mvs)
+        board.make_move(move)
+        disp_coins()
+    root.mainloop()
